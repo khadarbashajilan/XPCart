227e00cef8398ebb145f1360c5ee80619dd71b47
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-07-16 18:13:00 +0530

    implemented Product page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,87 +1,98 @@
-import React, { useState } from 'react'
-import { products } from '../data/products'
-import { Link } from 'react-router-dom'
-import { ShoppingCart, Star } from 'lucide-react'
+import React, { useState } from "react";
+import { products } from "../data/products";
+import { Link } from "react-router-dom";
+import { ShoppingCart, Star } from "lucide-react";
 
-interface CartItem{
-  ProductId:number;
-  quantity:number;
+interface CartItem {
+  ProductId: number;
+  quantity: number;
 }
 
 const HomePage = () => {
+  const [cart, setcart] = useState<CartItem[]>([]);
 
-  const [cart,setcart] = useState<CartItem[]>([]);
+  const prodcount = cart.reduce((sum, prod) => sum + prod.quantity, 0);
 
-  const prodcount = cart.reduce((sum,prod)=> sum+ prod.quantity,0);
-
-  function addToCart(id:number){
+  function addToCart(id: number) {
     setcart(
-      cart.some((item)=> item.ProductId === id)
-      ? cart.map((item)=>
-        item.ProductId === id
-        ? {...item, quantity:item.quantity+1 }
-          :item
-        )
-    : [...cart, { ProductId:id, quantity:1}]
+      cart.some((item) => item.ProductId === id)
+        ? cart.map((item) =>
+            item.ProductId === id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        : [...cart, { ProductId: id, quantity: 1 }]
     );
   }
   return (
     <>
-    <header className='p-6 sticky top-0 z-50 backdrop-blur-md'>
-      <nav>
-        <ul className='flex justify-between items-center'>
-          <li>
-            <Link to="/" className='text-2xl'>Home</Link>
-          </li>
-          <li>
-            <Link to="/cart" className='flex gap-x-2'>
-            <ShoppingCart/>
-            {prodcount>0 && <span className='text-white bg-[#2642c0] text-lg rounded-full w-8 h-8 flex items-center justify-center'>{prodcount}</span>}
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <header className="p-6 sticky top-0 z-50 backdrop-blur-md">
+        <nav>
+          <ul className="flex justify-between items-center">
+            <li>
+              <Link to="/" className="text-2xl">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart" className="flex gap-x-2">
+                <ShoppingCart />
+                {prodcount > 0 && (
+                  <span className="text-white bg-[#2642c0] text-lg rounded-full w-8 h-8 flex items-center justify-center">
+                    {prodcount}
+                  </span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-    <section className='flex flex-col gap-8'>
-      <h1 className='text-4xl font-bold'>Gaming Products</h1>
+      <section className="flex flex-col gap-8">
+        <h1 className="text-4xl font-bold">Gaming Products</h1>
 
-    <div className='grid gap-12 md:grid-cols-2 xl:grid-cols-3'>
-      {products.map((item,idx)=>(
-        <div key={idx}
-        className="flex flex-col gap-y-6  pl-4"
-        >
-          <Link to={`/${item.url}`}>
-          <figure className='h-96'>
-            <img src={item.image} alt={item.url} className="h-full w-full object-cover"/>
-          </figure>
-          </Link>
+        <div className="grid gap-12 md:grid-cols-2 xl:grid-cols-3">
+          {products.map((item, idx) => (
+            <div key={idx} className="flex flex-col gap-y-6  pl-4">
+              <Link to={`/${item.url}`}>
+                <figure className="h-96">
+                  <img
+                    src={item.image}
+                    alt={item.url}
+                    className="h-full w-full object-cover"
+                  />
+                </figure>
+              </Link>
 
-          <div className='space-y-2'>
-            <h3 className="text-xl text-[#3850ba]">{item.name}</h3>
+              <div className="space-y-2">
+                <h3 className="text-xl text-[#3850ba]">{item.name}</h3>
 
-            <div className='flex items-center gap-1'>
-            {
-              [...Array(5)].map((_,i)=>(
-                <Star key={i} className={`size-5 ${i<item.rating ? "text-yellow-400" : "text-white"}`}/>
-              ))
-            }
+                <div className="flex items-center gap-1">
+                  {[...Array(5)].map((_, i) => (
+                    <Star
+                      key={i}
+                      className={`size-5 ${
+                        i < item.rating ? "text-yellow-400" : "text-white"
+                      }`}
+                    />
+                  ))}
+                </div>
+
+                <p className="font-bold">{item.price}</p>
+              </div>
+
+              <button
+                onClick={() => addToCart(item.id)}
+                className="flex items-center justify-center gap-x-4 px-4 py-2 rounded-sm text-lg"
+              >
+                <ShoppingCart className="size-4" /> Add to Cart
+              </button>
             </div>
-
-            <p className='font-bold'>{item.price}</p>
-          </div>
-
-            <button
-            onClick={()=>addToCart(item.id)}
-            className='flex items-center justify-center gap-x-4 px-4 py-2 rounded-sm text-lg'
-            ><ShoppingCart className='size-4'/> Add to Cart</button>
-          </div>
-        ))}
-      </div>
-
-    </section>
+          ))}
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,11 +1,174 @@
-import React from 'react'
+import React, { useState } from "react";
+import { products } from "../data/products";
+import { Link, useParams } from "react-router-dom";
+import { ArrowRight, Minus, Plus, ShoppingCart, Star } from "lucide-react";
+import Reviews from "../components/Reviews";
 
-const ProductPage = () => {
-  return (
-    <div>
-      Productpage
-    </div>
-  )
+interface CartItem {
+  ProductId: number;
+  quantity: number;
 }
 
-export default ProductPage
+interface Reviews {
+  id: number;
+  user: string;
+  rating: number;
+  comment: string;
+  date: string;
+}
+
+interface productt {
+  id: number;
+  url: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  rating: number;
+  reviews: Reviews[];
+}
+const ProductPage = () => {
+  const { url } = useParams();
+  const product: productt | undefined = products.find(
+    (item) => item.url === url
+  );
+
+  if (!product) {
+    // Handle the case where product isn't found
+    throw new Error("Product not found");
+    // or return null/undefined, or show an error message
+  }
+
+  const [cart, setcart] = useState<CartItem[]>([]);
+
+  const prodcount = cart.reduce((sum, prod) => sum + prod.quantity, 0);
+
+  function getProductQuantity(id: number) {
+    return cart.find((item) => item.ProductId === id)?.quantity || 0;
+  }
+  const quantity = getProductQuantity(product.id);
+
+  function addToCart(id: number) {
+    setcart(
+      cart.some((item) => item.ProductId === id)
+        ? cart.map((item) =>
+            item.ProductId === id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        : [...cart, { ProductId: id, quantity: 1 }]
+    );
+  }
+  function removeFromCart(id: number) {
+    setcart((item) =>
+      item.map((item) =>
+        item.ProductId === id ? { ...item, quantity: item.quantity - 1 } : item
+      )
+    );
+  }
+
+  return (
+    <>
+      <header className="p-6 sticky top-0 z-50 backdrop-blur-md">
+        <nav>
+          <ul className="flex justify-between items-center">
+            <li>
+              <Link to="/" className="text-2xl">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart" className="flex gap-x-2">
+                <ShoppingCart />
+                {prodcount > 0 && (
+                  <span className="text-white bg-[#2642c0] text-lg rounded-full w-8 h-8 flex items-center justify-center">
+                    {prodcount}
+                  </span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section>
+        <h2>{product?.name}</h2>
+
+        <div className="flex gap-x-8 items-center ">
+          <div className="flex">
+            {[...Array(5)].map((_, i) => (
+              <Star
+                key={i}
+                className={`size-5 ${
+                  i < product.rating ? "text-yellow-400" : "text-gray-400"
+                }`}
+              />
+            ))}
+          </div>
+
+          <span className="text-xl">
+            {product.reviews.length}{" "}
+            {product.reviews.length === 1 ? "rating" : "ratings"}
+          </span>
+        </div>
+
+        <div className="grid gap-8 lg:grid-cols-2 lg:items-center">
+          <figure>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="rounded-sm"
+            />
+          </figure>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl">{product.description}</p>
+
+            <p className="text-xl">${product.price}</p>
+
+            <div className="grid gap-4 sm:grid-cols-[200px_1fr] lg:grid-cols-1">
+              <div className="flex justify-between border rounded-sm p-4 lg:w-fit lg:gap-x-8">
+                <button
+                  className="border  rounded-sm px-2 py-1 cursor-pointer disabled:bg-gray-700 disabled:border-transparent disabled:cursor-not-allowed"
+                  disabled={quantity === 0}
+                  onClick={() => removeFromCart(product.id)}
+                >
+                  <Minus className="size-5 text-white" />
+                </button>
+
+                <span>{quantity}</span>
+
+                <button
+                  className="  rounded-sm px-2 py-1 cursor-pointer"
+                  onClick={() => addToCart(product.id)}
+                >
+                  <Plus className="size-5 text-white" />
+                </button>
+              </div>
+
+              <div className="flex  flex-col gap-4 sm:flex-row">
+                <button
+                  onClick={() => addToCart(product.id)}
+                  className="flex items-center justify-center gap-x-2  px-4 py-2 rounded-sm"
+                >
+                  <ShoppingCart className="size-5" /> Add to Cart
+                </button>
+
+                <Link
+                  to="/cart"
+                  className="flex items-center justify-center gap-x-2 px-4 py-1 border  rounded-sm"
+                >
+                  Go to Cart <ArrowRight className="size-5" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Reviews reviews={product.reviews} />
+      </section>
+    </>
+  );
+};
+
+export default ProductPage;
